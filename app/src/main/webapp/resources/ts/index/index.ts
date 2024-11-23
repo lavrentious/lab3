cb0f476277bbaba01ce40a97320d00026cfc32d5
lavrentious <lavrent0@yandex.ru>
2024-11-23 13:48:30 +0300

    feat: clock on start page

--- app/src/main/webapp/resources/ts/index/index.ts
+++ app/src/main/webapp/resources/ts/index/index.ts
@@ -1,1 +1,15 @@
-console.log('index');
+const UPDATE_TIME_SECONDS = 5;
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("initializing clock");
+  const clock = document.getElementById("clock");
+
+  function time() {
+    const date = new Date();
+    const timeString = date.toLocaleTimeString();
+    clock.innerHTML = timeString;
+  }
+
+  setInterval(time, UPDATE_TIME_SECONDS * 1000);
+  time();
+});
